25430189a95e0cbe58a224acc080657621c94a34
lindwurm <lindwurm.q@gmail.com>
2020-05-16 01:52:09 +0900

    raphael: I am not a raphaelin
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -15,6 +15,6 @@ PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := lineage_raphael
 
-BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/raphael/raphael:10/QKQ1.190825.002/V11.0.1.0.QFKMIXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
